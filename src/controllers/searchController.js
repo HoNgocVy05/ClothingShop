3dc5a5470a18323837a62babd30b19f202d822be
NhuPhuc301 <phucnn5889@ut.edu.vn>
2025-11-14 01:48:53 +0700

    fix

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,24 +1,7 @@
 exports.getSearchResult = (req, res) => {
-  const q = (req.query.q || '').trim(); 
-  let products = mockProducts;
-
-  if (q) {
-    products = products.filter(p => 
-      p.name.toLowerCase().includes(q.toLowerCase())
-    );
-  }
-
-  res.render('user/searchResult', {
-    layout: './layouts/userMaster',
-    title: `Tìm kiếm: ${q || 'Tất cả'} - VPQ Studio`,
-    
-    isSearch: true,
-    searchQuery: q,        
-    products: products,    
-    
-    breadcrumbs: [
-      { text: 'Trang chủ', link: '/' },
-      { text: 'Tìm kiếm', link: '/search' }
-    ]
-  });
+    res.render('user/searchResult', {layout: './layouts/userMaster',title: 'VPQ Studio - Tìm kiếm',  
+        isSearch: true,                  
+        searchQuery: '',                 
+        products: []                     
+    });
 };
