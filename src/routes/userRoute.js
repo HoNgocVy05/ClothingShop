68b13485766a589397401280117209335ed8ed5c
NhuPhuc301 <phucnn5889@ut.edu.vn>
2025-11-15 20:25:47 +0700

    productInfor

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const index = require('../controllers/index');
 const searchController = require('../controllers/searchController');
+const productInforController = require('../controllers/productInforController')
 
 // các trang user
 router.get('/', index.getIndex);
 router.get('/search',searchController.getSearchResult)
+router.get('/product',productInforController.getProductInfor)
 
 module.exports = router;
