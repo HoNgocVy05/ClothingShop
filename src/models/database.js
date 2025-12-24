79885c57b2abc1f6eb8fe8e83a0fef2a3c28ba1a
HoNgocVy05 <vyho.2022005@gmail.com>
2025-12-24 18:10:04 +0700

    add responsive

--- src/models/database.js
+++ src/models/database.js
@@ -1,34 +1,34 @@
-// require('dotenv').config(); 
-// const mysql = require('mysql2');
-// const mySqlPort = process.env.mySqlPort;
-// const mySqlHost = process.env.mySqlHost;
-// const mySqlUser = process.env.mySqlUser;
-// const mySqlPassword = process.env.mySqlPassword;
-// const mySqlDb = process.env.mySqlDb;
+require('dotenv').config(); 
+const mysql = require('mysql2');
+const mySqlPort = process.env.mySqlPort;
+const mySqlHost = process.env.mySqlHost;
+const mySqlUser = process.env.mySqlUser;
+const mySqlPassword = process.env.mySqlPassword;
+const mySqlDb = process.env.mySqlDb;
 
-// const pool = mysql.createPool({
-//     host: mySqlHost,
-//     user: mySqlUser,
-//     password: mySqlPassword,
-//     database: mySqlDb,
-//     port: mySqlPort
-// }).promise();
-
-
-// (async () => {
-//     try {
-//         const [rows] = await pool.query("SELECT 1");
-//         console.log("Kết nối db thành công!");
-//     } catch (err) {
-//         console.log("Kết nối db thất bại:", err);
-//     }
-// })();
-
-// module.exports = pool;
+const pool = mysql.createPool({
+    host: mySqlHost,
+    user: mySqlUser,
+    password: mySqlPassword,
+    database: mySqlDb,
+    port: mySqlPort
+}).promise();
 
 
-const mysql = require('mysql2/promise');
-
-const pool = mysql.createPool(process.env.MYSQL_URL);
+(async () => {
+    try {
+        const [rows] = await pool.query("SELECT 1");
+        console.log("Kết nối db thành công!");
+    } catch (err) {
+        console.log("Kết nối db thất bại:", err);
+    }
+})();
 
 module.exports = pool;
+
+
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool(process.env.MYSQL_URL);
+
+// module.exports = pool;
